173268193d3d7a3e7a12a3ff431fdc8a7c0b7613
Anurag Anand <anuraganand789@hotmail.com>
2020-11-08 02:50:18 +0530

    code modified to use OHLCEvent class and print the logs of incoming data

--- ohlc/src/main/java/com/upstox/queue/OHLCBarDataQueue.java
+++ ohlc/src/main/java/com/upstox/queue/OHLCBarDataQueue.java
@@ -1,20 +1,20 @@
 package com.upstox.queue;
 
-import com.upstox.model.OHLCData;
+import com.upstox.model.OHLCEvent;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.logging.Logger;
 
 public class OHLCBarDataQueue{
     private static final Logger LOGGER = Logger.getLogger(OHLCBarDataQueue.class.getName());
-    //TODO : run code with loggr to print the data with regular interval
-    private static final int INITIALSIZE = 10_000;
-    private static final ArrayBlockingQueue<OHLCData> ohlcBarData = new ArrayBlockingQueue(INITIALSIZE);
+    private static final int INITIALSIZE = 1_000;
+    private static final ArrayBlockingQueue<OHLCEvent> ohlcBarData = new ArrayBlockingQueue(INITIALSIZE);
 
-    public OHLCData read() throws InterruptedException{
+    public static OHLCEvent read() throws InterruptedException{
         return ohlcBarData.take();
     }
 
-    public void write(final OHLCData ohlcData) throws InterruptedException{
-        ohlcBarData.put(ohlcData);
+    public static void write(final OHLCEvent ohlcEvent) throws InterruptedException{
+	LOGGER.info(ohlcEvent.toJSONString());
+        ohlcBarData.put(ohlcEvent);
     }
 }
